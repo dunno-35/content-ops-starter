b8c563bd20790fd598545f94a313994652cae6c3
Alex Mardle <alex.mardle@icloud.com>
2025-11-14 19:31:30 +0000

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,13 +5,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        {/* Basic favicon */}
         <link rel="icon" href="/favicon.png" type="image/png" />
-
-        {/* Optional: multiple sizes for better device support */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
       <Component {...pageProps} />
     </>
