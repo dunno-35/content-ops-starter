169aaf17bcae8a44d8eebc0ea2b329914c1e4af9
Alex Mardle <alex.mardle@icloud.com>
2025-11-14 19:24:47 +0000

    Include favicon links in MyApp component
    Added Head component for favicon support.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,19 @@
 import '../css/main.css';
+import Head from 'next/head';
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        {/* Basic favicon */}
+        <link rel="icon" href="/favicon.png" type="image/png" />
+
+        {/* Optional: multiple sizes for better device support */}
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
