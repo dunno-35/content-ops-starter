c79281fffff8926a057bc73d73644b318827361e
Alex Mardle <alex.mardle@icloud.com>
2026-01-26 15:36:50 +0000

    Fix formatting and ensure consistent export in next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,18 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    env: {
-        stackbitPreview: process.env.STACKBIT_PREVIEW
-    },
-    trailingSlash: true,
-    reactStrictMode: true,
-    allowedDevOrigins: [
-        '192.168.1.84'
-    ]
+  output: 'export',
+
+  env: {
+    stackbitPreview: process.env.STACKBIT_PREVIEW
+  },
+
+  trailingSlash: true,
+  reactStrictMode: true,
+
+  allowedDevOrigins: [
+    '192.168.1.84'
+  ]
 };
 
 module.exports = nextConfig;
